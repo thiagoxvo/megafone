1796ecf5193cf89bf77e8e8644c2dd770096c705
Thiago Xavier <thiagoxvo@gmail.com>
2014-08-20 18:37:02 -0300

    Add socket.io

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http')
 var port = process.env.PORT || 5000
-// var io = require('socket.io')(http);
+var io = require('socket.io')(http);
 
 app.use('/', express.static(__dirname + '/public'));
 
@@ -11,24 +11,11 @@ app.get('/', function(req, res){
 });
 
 app.get('/play', function(req, res){
-  // io.emit('play-sound');
+  io.emit('play-sound');
   res.send("Life is such!");
 })
 
 var server = http.createServer(app)
 server.listen(port)
 
-console.log("http server listening on %d", port)
-
-// wss.on("connection", function(ws) {
-//   var id = setInterval(function() {
-//     ws.send(JSON.stringify(new Date()), function() {  })
-//   }, 1000)
-
-//   console.log("websocket connection open")
-
-//   ws.on("close", function() {
-//     console.log("websocket connection close")
-//     clearInterval(id)
-//   })
-// })
+console.log("http server listening on %d", port);
