e17e223095d5b5d51693bb2526ddd617904456c2
Thiago Xavier <thiagoxvo@gmail.com>
2014-08-22 00:05:45 -0300

    Add a parameter with sound name to be played

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var _ = require('underscore');
+
+var sounds = [{
+  name : "sadtrombone",
+  file : "sad_trombone.mp3"
+}];
+
+var getSoundFile = function(sound){
+  return _.findWhere(sounds, {name : sound}).file;
+};
 
 app.use('/', express.static(__dirname + '/public'));
 
@@ -9,9 +19,11 @@ app.get('/', function(req, res){
   res.sendfile('public/index.html');
 });
 
-app.get('/play', function(req, res){
-  io.emit('play-sound');
-  res.send("Life is such!");
+app.get('/play/:sound', function(req, res) {
+  var fileName = getSoundFile(req.params.sound);
+  console.log(fileName);
+  io.emit('play-sound', {fileName : fileName });
+  res.send("Playing... " + fileName);
 })
 
 var port = process.env.PORT || 3000
